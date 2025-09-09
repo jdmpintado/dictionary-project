d2641def3a5a3e47c88aa1a337dbe56d3ad2109e
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-09-09 11:38:59 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://www.shecodes.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logo} className="App-logo img-fluid" alt="logo" />
+          </a>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This project was coded by Jéssica Pintado, and is{" "}
+          <a
+            className="GitHub repo"
+            href="https://github.com/jdmpintado/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-source
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }

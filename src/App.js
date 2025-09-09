4f333c2bb3d98b80c28d35a109d1622824d69af5
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-09-09 10:44:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://www.shecodes.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
